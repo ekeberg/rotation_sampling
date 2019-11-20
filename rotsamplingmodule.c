b6ac0314cd258272adc15af2188c74808abbac68
Tomas Ekeberg <tomas.ekeberg@gmail.com>
2019-11-20 10:03:37 +0100

    Replaced Py_InitModule3 with newer system

--- rotsamplingmodule.c
+++ rotsamplingmodule.c
@@ -4,6 +4,13 @@
 #include <math.h>
 #include <stdio.h>
 
+#define NPY_NO_DEPRECATED_API NPY_1_7_API_VERSION
+
+#if PY_MAJOR_VERSION >= 3
+#define IS_PY3
+#endif
+
+
 const float tau = 1.6180339887498949; // the golden ratio
 
 typedef struct{
@@ -435,10 +442,72 @@ static PyMethodDef RotsamplingMethods[] = {
   {NULL, NULL, 0, NULL}
 };
 
-PyMODINIT_FUNC initrotsampling(void)
+
+#ifdef IS_PY3
+  #define MOD_ERROR_VAL NULL
+  #define MOD_SUCCESS_VAL(val) val
+  #define MOD_INIT(name) PyMODINIT_FUNC PyInit_##name(void)
+  #define MOD_DEF(ob, name, doc, methods) \
+          static struct PyModuleDef moduledef = { \
+	    PyModuleDef_HEAD_INIT, name, doc, -1, methods, }; \
+	  ob = PyModule_Create(&moduledef);
+#else
+  #define MOD_ERROR_VAL
+  #define MOD_SUCCESS_VAL(val)
+  #define MOD_INIT(name) void init##name(void)
+  #define MOD_DEF(ob, name, doc, methods) \
+          ob = Py_InitModule3(name, methods, doc);
+#endif
+
+MOD_INIT(rotsampling)
 {
   import_array();
-  PyObject *m = Py_InitModule3("rotsampling", RotsamplingMethods, "Sample rotation space");
-  if (m == NULL)
-    return;
+  PyObject *module;
+  MOD_DEF(module, "rotsampling", "Sample rotation space.", RotsamplingMethods)
+  if (module == NULL)
+    return MOD_ERROR_VAL;
+  return MOD_SUCCESS_VAL(module);
 }
+
+
+
+/* #ifdef IS_PY3 */
+/* static struct PyModuleDef moduledef = { */
+/*   PyModuleDef_HEAD_INIT, */
+/*   "rotmodule",      /\* m_name *\/ */
+/*   "Sample rotation space.", /\* m_doc *\/ */
+/*   -1,          /\* m_size *\/ */
+/*   RotsamplingMethods, /\* m_methods *\/ */
+/*   NULL,        /\* m_reload *\/ */
+/*   NULL,        /\* m_traverse *\/ */
+/*   NULL,        /\* m_clear *\/ */
+/*   NULL,        /\* m_free *\/ */
+/* }; */
+/* #endif */
+
+/* #ifdef IS_PY3 */
+/*   PyMODINIT_FUNC PyInit_rotsampling(void) */
+/* #else */
+/*   PyMODINIT_FUNC initrotsampling(void) */
+/* #endif */
+/* { */
+/*   import_array(); */
+
+/*   #ifdef IS_PY3 */
+/*     PyObject *module = PyModule_Create(&moduledef); */
+/*   #else */
+/*     PyObject *module = Py_InitModule3("rotsampling", RotsamplingMethods, "Sample rotation space"); */
+/*   #endif */
+
+/*   if (module == NULL) { */
+/*     #ifdef IS_PY3 */
+/*       return NULL; */
+/*     #else */
+/*       return; */
+/*     #endif */
+/*   } */
+  
+/*   #ifdef IS_PY3 */
+/*     return module; */
+/*   #endif */
+/* } */
--- setup.py
+++ setup.py
@@ -3,4 +3,4 @@ import numpy.distutils.misc_util
 
 ext = Extension("rotsampling", sources=["rotsamplingmodule.c"], include_dirs=[numpy.get_include()])
 #setup(ext_modules=[ext], include_dirs=numpy.distutils.misc_util.get_numpy_include_dirs())
-setup(ext_modules=[ext], include_dirs=[numpy.get_include()])
+setup(name="rotsampling", ext_modules=[ext], include_dirs=[numpy.get_include()])
